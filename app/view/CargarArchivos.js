f9a3c19e73433498137af8d3db566b1ffb176a10
Axelxavier <harry.carreno@gmail.com>
2014-10-08 23:38:07 -0500

    cargar archivos
    actualizo el nuevo formato de carga de archivos

--- app/view/CargarArchivos.js
+++ app/view/CargarArchivos.js
@@ -2,6 +2,50 @@
  * Created by Toshiba on 26/09/14.
  */
 
+//Ext.regModel('modelo', {
+Ext.define('modelo', {
+    extend: 'Ext.data.Model',
+    fields: [
+        {name: 'ldcnomc'},
+        {name: 'ldcnomfilc'},
+        {name: 'ldcstac'},
+        {name: 'ldctamc'},
+        {name: 'sysldcfecd'}
+    ]
+});
+
+var Nstore = new Ext.data.Store({
+    model:'modelo',
+    fields: [
+        {name: 'Nombre'},
+        {name: 'Nombre Archivo'},
+        {name: 'status Carga'},
+        {name: 'Tamaño Archivo'},
+        {name: 'Fecha Carga'}
+    ],
+    data: [
+        ['Ext JS 4: First Look','Loiane Groner'],
+        ['Ext JS 4: First Look','Loiane Groner'],
+        ['Ext JS 4: First Look','Loiane Groner'],
+        ['Ext JS 4: First Look','Loiane Groner'],
+        ['Ext JS 4: First Look','Loiane Groner']    
+    ]
+});
+
+//var pluginExpanded = true;
+var Ngrid = Ext.create('Ext.grid.Panel', {
+   store: Nstore,
+   columns: [ 
+        {text : 'id',  width:50, sortable : false},
+        {text : 'Nombre', flex : 1, sortable : true , dataIndex: 'ldcnomc'},
+        {text : 'Nombre Archivo', flex : 1, sortable : true , dataIndex: 'ldcnomfilc'},
+        {text : 'Status Carga', flex : 1, sortable : true , dataIndex: 'ldcstac'},
+        {text : 'Tamaño Carga', flex : 1, sortable : true , dataIndex: 'ldctamc'},
+        {text : 'Fecha Carga', flex : 1, sortable : true , dataIndex: 'sysldcfecd'}
+    ],        
+    title: 'Archivos Cargados'
+});
+
 // Inicio Formulario Cargar archivos
 Ext.create('Ext.form.Panel', {
     xtype: 'form',
@@ -9,80 +53,35 @@ Ext.create('Ext.form.Panel', {
     bodyPadding: 10,
     title: 'Cargar archivos',
     closable: true,
+    layout: 'anchor',
     items: [
         {
             xtype: 'fieldset',
             title: 'Maestro de productos',
+            height: 100,
             items: [
+                {
+                    xtype: 'textfield',
+                    anchor: '100%',
+                    name: 'nArchivo',
+                    fieldLabel: 'Nombre'
+                },
                 {
                     xtype: 'filefield',
                     anchor: '100%',
-                    name:'maestroProducto',
+                    name:'maestro',
                     fieldLabel: 'Archivo origen',
-                    buttonText: 'Abrir...'
-                },
-                {
-                    xtype: 'combobox',
-                    anchor: '100%',
-                    fieldLabel: 'Elegir hoja'
+                    buttonText: 'Abrir...',
+                    listeners: {
+                        change: function(fld, value) {
+                            var newValue = value.replace(/C:\\fakepath\\/g, '');
+                            fld.setRawValue(newValue);
+                        }
+                    }
                 }
             ]
-        },
-        {
-            xtype: 'fieldset',
-            title: 'Maestro de stock',
-            items: [
-                {
-                    xtype: 'filefield',
-                    anchor: '100%',
-                    name:'maestroStock',
-                    fieldLabel: 'Archivo origen',
-                    buttonText: 'Abrir...'
-                },
-                {
-                    xtype: 'combobox',
-                    anchor: '100%',
-                    fieldLabel: 'Elegir hoja'
-                }
-            ]
-        },
-        {
-            xtype: 'fieldset',
-            title: 'Maestro de valorizaciones',
-            items: [
-                {
-                    xtype: 'filefield',
-                    anchor: '100%',
-                    name:'maestroValor',
-                    fieldLabel: 'Archivo origen',
-                    buttonText: 'Abrir...'
-                },
-                {
-                    xtype: 'combobox',
-                    anchor: '100%',
-                    fieldLabel: 'Elegir hoja'
-                }
-            ]
-        },
-        {
-            xtype: 'fieldset',
-            title: 'Maestro de lectura',
-            items: [
-                {
-                    xtype: 'filefield',
-                    anchor: '100%',
-                    name:'maestroLectura',
-                    fieldLabel: 'Archivo origen',
-                    buttonText: 'Abrir...'
-                },
-                {
-                    xtype: 'combobox',
-                    anchor: '100%',
-                    fieldLabel: 'Elegir hoja'
-                }
-            ]
-        }
-    ],
+        },Ngrid        
+    ],    
     dockedItems: [
         {
             xtype: 'toolbar',
--- svr/src/cargarMaestros.php
+++ svr/src/cargarMaestros.php
@@ -9,120 +9,37 @@ date_default_timezone_set('America/Lima');
 //$fecha_de_hoy=date('_Y-m-d_h-i-s_a');
 
 //cargar el phpexcel
-require_once "../lib/PHPExcel/IOFactory.php";
+//require_once "../lib/PHPExcel/IOFactory.php";
+$nombre=$_POST['nArchivo'];
+$maestro=$_FILES['maestro']['name'];
+$tamanio=$_FILES['maestro']['size'];
+//$maestroStock=$_FILES['maestroStock']['name'];
+//$maestroValor=$_FILES['maestroValor']['name'];
+//$maestroLectura=$_FILES['maestroLectura']['name'];
 
-$maestroProducto=$_FILES['maestroProducto']['name'];
-$maestroStock=$_FILES['maestroStock']['name'];
-$maestroValor=$_FILES['maestroValor']['name'];
-$maestroLectura=$_FILES['maestroLectura']['name'];
-
+//$ext=array_pop(explode(".",$maestro));
 $directorio = "../storage/";
+$nombreArchivo = date('Ymd_His_').$maestro;
+$statusCarga = "Correcto";
+move_uploaded_file($_FILES['maestro']['tmp_name'],$directorio.$nombreArchivo);
 
 //cargamos el phpexcel
+/*
 $objReader = PHPExcel_IOFactory::createReader('Excel2007');
 $objReader->setReadDataOnly(true);
+*/
+
+$sql="insert into ldc (ldcnomc,ldcnomfilc,ldcstac,ldctamc) values ";
+$sql.="(";
+$sql.="\"".$nombre."\",";
+$sql.="\"".$nombreArchivo."\",";
+$sql.="\"".$statusCarga."\",";
+$sql.="\"".$tamanio."\"";
+$sql.=")";
+
 mysql_connect("localhost","root","d3v3l0p3r");
 mysql_select_db("tomasdb") or die ("no se puede conectar a la bd");
-
-if ($maestroProducto!="") {
-	move_uploaded_file($_FILES['maestroProducto']['tmp_name'],$directorio.$maestroProducto);			
-	$objPHPExcel = $objReader->load($directorio.$maestroProducto);
-	$objWorksheet = $objPHPExcel->getActiveSheet(0);//indicamos que empezamos con la primer hoja
-	$highestRow = $objWorksheet->getHighestRow(); //Leemos cuantas filas tiene
-	if(!$highestRow){//si tiene 0 filas
-	  die("El archivo de excel no contiene informacion o bien esta no es accesible");
-	}
-	$sql="insert into pro (procodc,proskuc,prodesc,proundc) values ";
-	for ($i=2; $i<$highestRow+1 ; $i++) { //filas
-		$sql.="(";
-		for ($j=1; $j<5 ; $j++) { //columnas
-			$valorcampo=$objWorksheet->getCellByColumnAndRow($j, $i)->getValue();
-			$sql.="\"".$valorcampo."\",";
-		}
-		$sql = substr ($sql,0,-1);
-		$sql.="),";
-	}
-	$sql = substr ( $sql, 0, - 1 );
-	mysql_query($sql) or die(mysql_error());
-	$descripcion='{archivo:'.$maestroProducto.',registros:'.($highestRow-1).'}';
-	//$sqllog="insert into log (logdesc,logaccc) values (\"frmCargarMaestros\",\"".$descripcion."\")";
-	//mysql_query($sqllog) or die(mysql_error());
-}
-
-if ($maestroStock!="") {
-	move_uploaded_file($_FILES['maestroStock']['tmp_name'],$directorio.$maestroStock);			
-	$objPHPExcel = $objReader->load($directorio.$maestroStock);
-	$objWorksheet = $objPHPExcel->getActiveSheet(0);//indicamos que empezamos con la primer hoja
-	$highestRow = $objWorksheet->getHighestRow(); //Leemos cuantas filas tiene
-	if(!$highestRow){//si tiene 0 filas
-	  die("El archivo de excel no contiene informacion o bien esta no es accesible");
-	}
-	$sql="insert into stk (stkcodc,stkskuc,stkubic,stkundc,stkstkf) values ";
-	for ($i=2; $i<$highestRow+1 ; $i++) { //filas
-		$sql.="(";
-		for ($j=1; $j<6 ; $j++) { //columnas
-			$valorcampo=$objWorksheet->getCellByColumnAndRow($j, $i)->getValue();
-			$sql.="\"".$valorcampo."\",";
-		}
-		$sql = substr ($sql,0,-1);
-		$sql.="),";
-	}
-	$sql = substr ( $sql, 0, - 1 );
-	mysql_query($sql) or die(mysql_error());
-	$descripcion='{archivo:'.$maestroStock.',registros:'.($highestRow-1).'}';
-	$sqllog="insert into log (logdesc,logaccc) values (\"frmCargarMaestros\",\"".$descripcion."\")";
-	//mysql_query($sqllog) or die(mysql_error());
-}
-
-if ($maestroValor!="") {
-	move_uploaded_file($_FILES['maestroValor']['tmp_name'],$directorio.$maestroValor);			
-	$objPHPExcel = $objReader->load($directorio.$maestroValor);
-	$objWorksheet = $objPHPExcel->getActiveSheet(0);//indicamos que empezamos con la primer hoja
-	$highestRow = $objWorksheet->getHighestRow(); //Leemos cuantas filas tiene
-	if(!$highestRow){//si tiene 0 filas
-	  die("El archivo de excel no contiene informacion o bien esta no es accesible");
-	}
-	$sql="insert into val (valcodc,valskuc,valubic,valundc,valstkf,valstvf) values ";
-	for ($i=2; $i<$highestRow+1 ; $i++) { //filas
-		$sql.="(";
-		for ($j=1; $j<7 ; $j++) { //columnas
-			$valorcampo=$objWorksheet->getCellByColumnAndRow($j, $i)->getValue();
-			$sql.="\"".$valorcampo."\",";
-		}
-		$sql = substr ($sql,0,-1);
-		$sql.="),";
-	}
-	$sql = substr ( $sql, 0, - 1 );
-	mysql_query($sql) or die(mysql_error());
-	$descripcion='{archivo:'.$maestroValor.',registros:'.($highestRow-1).'}';
-	$sqllog="insert into log (logdesc,logaccc) values (\"frmCargarMaestros\",\"".$descripcion."\")";
-	//mysql_query($sqllog) or die(mysql_error());
-}
-
-if ($maestroLectura!="") {
-	move_uploaded_file($_FILES['maestroLectura']['tmp_name'],$directorio.$maestroLectura);			
-	$objPHPExcel = $objReader->load($directorio.$maestroLectura);
-	$objWorksheet = $objPHPExcel->getActiveSheet(0);//indicamos que empezamos con la primer hoja
-	$highestRow = $objWorksheet->getHighestRow(); //Leemos cuantas filas tiene
-	if(!$highestRow){//si tiene 0 filas
-	  die("El archivo de excel no contiene informacion o bien esta no es accesible");
-	}
-	$sql="insert into pro (procodc,proskuc,prodesc,proundc) values ";
-	for ($i=2; $i<$highestRow+1 ; $i++) { //filas
-		$sql.="(";
-		for ($j=1; $j<5 ; $j++) { //columnas
-			$valorcampo=$objWorksheet->getCellByColumnAndRow($j, $i)->getValue();
-			$sql.="\"".$valorcampo."\",";
-		}
-		$sql = substr ($sql,0,-1);
-		$sql.="),";
-	}
-	$sql = substr ( $sql, 0, - 1 );
-	mysql_query($sql) or die(mysql_error());
-	$descripcion='{archivo:'.$maestroLectura.',registros:'.($highestRow-1).'}';
-	$sqllog="insert into log (logdesc,logaccc) values (\"frmCargarMaestros\",\"".$descripcion."\")";
-	//mysql_query($sqllog) or die(mysql_error());
-}
+mysql_query($sql) or die(mysql_error());
 
 echo "{success:true}";
 ?>
